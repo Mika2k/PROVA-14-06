3e20912d46665800bcca53561c2694987b69c4ec
Mika2k <mikael.saldanha@tecpuc.com.br>
2024-06-14 21:24:15 -0300

    Update programa_0706.py

--- programa_0706.py
+++ programa_0706.py
@@ -1,32 +1,42 @@
 import pandas as pd  
 import matplotlib.pyplot as plt
 import streamlit as st
-
+import ipeadatapy as ip
+ 
+ 
 st.set_page_config(
-
-page_title="Empresa 1", 
+  page_title="Projeto", 
+page_icon="👋",
 )
-
-st.header("Dados da empresa 1")
-
-arquivo = "https://raw.githubusercontent.com/WesleyInfoBr2/aula_teste/main/empresa1.csv" 
-dfe = pd.read_csv(arquivo, sep=';') 
-st.dataframe(dfe.head(3))
-
+ 
+st.header("Dados do Projeto")
+ 
+arquivo = "https://raw.githubusercontent.com/Mika2k/PROVA-14-06/main/projetos-1.csv"
+df = pd.read_csv(arquivo, sep=';') 
+st.dataframe(df.head(23))
+ 
 st.write("Gráfico de linha dos indicadores ao longo do tempo")
-
+ 
 fig, ax = plt.subplots()
-dfe.plot(ax=ax)
+df.plot(ax=ax)
 st.pyplot(fig)
-
+ 
 fig, ax = plt.subplots()
-dfe.plot(kind = 'scatter', x = 'EBITDA', y = 'Lucro operacional', ax=ax)
+df.plot(kind = 'scatter', x = 'Projeto1', y = 'Projeto2', ax=ax)
 st.pyplot(fig)
-
+ 
 fig, ax = plt.subplots()
-dfe["Lucro do período"].plot(kind = 'hist', ax=ax)
+df["Projeto1"].plot(kind = 'hist', ax=ax)
+df["Projeto4"].plot(kind = 'hist', ax=ax)
 st.pyplot(fig)
-
-
-st.write(dfe.groupby('Ano').mean())
-
+ 
+st.header("Ipea Selic")
+ 
+dados = ip.list_series('Selic')
+dados
+selic = ip.timeseries('BM12_TJOVER12', yearGreaterThan=2021, yearSmallerThan=2024)
+selic
+fig, ax = plt.subplots()
+ip.timeseries('BM12_TJOVER12', year=2021).plot("MONTH", "VALUE ((% a.m.))", ax=ax)
+ip.timeseries('BM12_TJOVER12', year=2022).plot("MONTH", "VALUE ((% a.m.))", ax=ax)
+st.pyplot(fig)
